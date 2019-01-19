40a4376fa4f078a3f9512b75514573c4aa342a06
Alex <a.matulionis@usa.com>
2019-01-18 18:03:46 -0800

    Added injection list.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react'
+
 import Button from '@material-ui/core/Button'
 import Dialog from '@material-ui/core/Dialog'
+import FileCopy from '@material-ui/icons/FileCopy'
+import TextField from '@material-ui/core/TextField'
+import Fab from '@material-ui/core/Fab'
+
 import mammoth from 'mammoth'
+
 import './styles/App.css'
 
 class App extends Component {
@@ -9,43 +15,87 @@ class App extends Component {
   state = {}
   
   render() {
-    const { letter } = this.state
-    
+    const { template, letter } = this.state
+
+    const keyWords = (template||'').match(/\b[A-Z_]{3,}\b/g)
+
     return (
-      <div className="App">
+      <div className={template? 'App-2' : 'App'}>
 
-        <input
-          hidden
-          multiple
-          accept =".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-          id     ="upload-button"
-          type   ="file"
+        {
+          template? <>
+            {
+              keyWords.map((keyWord, i) =>
+                <TextField
+                  key={i}
+                  required
+                  margin="normal"
+                  label={keyWord}
+                  value={this.state[keyWord] || ''}
+                  onChange={e => this.setState({ [keyWord]: e.target.value })}
+                />
+              )
+            }
 
-          /** process uploaded file */
-          onChange={async e => {
-            const blob = e.target.files[0]
-            const arrayBuffer = await new Response(blob).arrayBuffer()
-            
-            const { value, messages } = await mammoth.extractRawText({ arrayBuffer })
-            if (messages && messages.length) console.warn(messages)
-            console.log(value)
+            <Fab
+              style={{
+                position: `fixed`,
+                right: 0,
+                bottom: 0,
+                margin: `1em`,
+              }}
+              color="primary"
+              aria-label="Copy"
 
-            this.setState({ letter: value })
-          }}
+              onClick={e => this.setState({
+                letter: Object.keys(this.state)
+                  .filter(key => key.replace('_', '').toUpperCase() === key.replace('_', ''))
+                  .reduce(
+                    (letterInProgress, keyWord) => letterInProgress.replace(keyWord, this.state[keyWord]),
+                    template
+                  )
+              })}
+            >
+              <FileCopy />
+            </Fab>
 
-        /><label htmlFor="upload-button">
+            <Dialog open={!!letter} onClose={() => {}}>
+              <pre>
+                {letter}
+              </pre>
+            </Dialog>
 
-          <Button variant="contained" component="span">
-            Upload Template
-          </Button>
-          
-        </label>
+          </> : <>
 
-        <Dialog open={!!letter}>
-          <pre>
-            {letter}
-          </pre>
-        </Dialog>
+            <input
+              hidden
+              multiple
+              accept =".doc,.docx,.xml,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+              id     ="upload-button"
+              type   ="file"
+
+              /** process uploaded file */
+              onChange={async e => {
+                const blob = e.target.files[0]
+                const arrayBuffer = await new Response(blob).arrayBuffer()
+                
+                const { value, messages } = await mammoth.extractRawText({ arrayBuffer })
+                if (messages && messages.length) console.warn(messages)
+                console.log(value)
+
+                this.setState({ template: value })
+              }}
+
+            /><label htmlFor="upload-button">
+
+              <Button variant="contained" component="span">
+                Upload Template
+              </Button>
+              
+            </label>
+
+          </>
+        }
       
       </div>
     )
